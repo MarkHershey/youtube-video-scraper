2bb55d829b79f1660b9fbabbab25e3658e799a7a
Mark Huang <hhzeos@gmail.com>
2020-06-03 23:23:35 +0800

    Update getVideoInfo

--- getVideoInfo.py
+++ getVideoInfo.py
@@ -5,36 +5,46 @@
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
 import os
+import json
+import logging
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
+from developer_key import KEY
+
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
-def main():
+def get_video_info_by_id(id: str):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    # client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    #
+    # # Get credentials and create an API client
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes
+    # )
+    # credentials = flow.run_console()
+    # youtube = googleapiclient.discovery.build(
+    #     api_service_name, api_version, credentials=credentials
+    # )
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes
-    )
-    credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials
+        api_service_name, api_version, developerKey=KEY
     )
 
-    request = youtube.videos().list(part="contentDetails,status", id="gHtrPzhKEhk")
+    request = youtube.videos().list(part="contentDetails,status", id=id)
     response = request.execute()
 
-    print(response)
+    duration = response["items"][0]["contentDetails"]
+    license = response["items"][0]["status"]["license"]
+    return {"duration": duration, "license": license}
 
 
 if __name__ == "__main__":
---
+++ sample_files/video-info-response.py
@@ -0,0 +1,29 @@
+{
+    "kind": "youtube#videoListResponse",
+    "etag": "0Ot5npSDBhluNYRp_3Lf4uwQUZw",
+    "items": [
+        {
+            "kind": "youtube#video",
+            "etag": "bjVuGrMGACk2cZ2wZP6lhYaaq4M",
+            "id": "gHtrPzhKEhk",
+            "contentDetails": {
+                "duration": "PT10M20S",
+                "dimension": "2d",
+                "definition": "sd",
+                "caption": "false",
+                "licensedContent": False,
+                "contentRating": {},
+                "projection": "rectangular",
+            },
+            "status": {
+                "uploadStatus": "processed",
+                "privacyStatus": "public",
+                "license": "youtube",
+                "embeddable": True,
+                "publicStatsViewable": True,
+                "madeForKids": False,
+            },
+        }
+    ],
+    "pageInfo": {"totalResults": 1, "resultsPerPage": 1},
+}
