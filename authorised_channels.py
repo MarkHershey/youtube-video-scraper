6294b93c64f3394ac40a918a975ff7fe715edb75
Mark Huang <hhzeos@gmail.com>
2020-06-25 23:22:46 +0800

    Update merge list

---
+++ authorised_channels.py
@@ -0,0 +1,4 @@
+authorised_channels = {
+    "UCygISJtog2MXdhaHhXFBAGw": "DDS TV",
+    "UCgMzWjqVXPjSAvv7qxNhauw": "Thunder Cams",
+}
--- merge_video_lists.py
+++ merge_video_lists.py
@@ -2,6 +2,8 @@ import os
 import json
 from pathlib import Path
 
+from authorised_channels import authorised_channels
+
 
 def merge(folder: str = "", debug: bool = False):
 
@@ -19,33 +21,81 @@ def merge(folder: str = "", debug: bool = False):
             return
 
     video_lst = dict()
-    video_count = 0
-    debug = False
+    total_video_count = 0
 
-    for dirpath, dirnames, filenames in os.walk(FOLDER_PATH, topdown=True):
-        if debug:
-            print("-------------------------------------------------------------------")
-            print(f"Current directory: {dirpath}")
-            print(f"# sub-directories: {len(dirnames)}")
-            print(f"# files in current directory: {len(filenames)}\n")
+    authorised_video_lst = dict()
+    authorised_video_count = 0
 
-        for index, filename in enumerate(filenames):
-            # if filename[-5:] == ".json" and filename[:10] == "video_lst_":
-            # if filename[-5:] == ".json" and filename[:18] == "channel_video_lst_":
-            if filename[-5:] == ".json":
-                print(os.path.join(dirpath, filename))
-                with open(os.path.join(dirpath, filename), "r") as file:
-                    video_lst_tmp = json.load(file)
-                    for id, content in video_lst_tmp.items():
-                        if id in video_lst:
-                            pass
+    channel_lst = dict()
+    total_channel_count = 0
+    authorised_channel_count = len(authorised_channels.keys())
+
+    for index, filename in enumerate(os.listdir(FOLDER_PATH)):
+        # if filename[-5:] == ".json" and filename[:10] == "video_lst_":
+        # if filename[-5:] == ".json" and filename[:18] == "channel_video_lst_":
+        if filename[-5:] == ".json":
+            filepath = FOLDER_PATH / filename
+
+            with filepath.open() as file:
+                # load the sub list
+                video_lst_tmp = json.load(file)
+                for id, content in video_lst_tmp.items():
+
+                    channelTitle = content["channelTitle"]
+                    channelId = content["channelId"]
+
+                    if id in video_lst:
+                        pass
+                    else:
+                        video_lst[id] = content
+                        total_video_count += 1
+
+                        if channelId in authorised_channels.keys():
+                            authorised_video_lst[id] = content
+                            authorised_video_count += 1
+
+                        if channelId in channel_lst:
+                            channel_lst[channelId]["downloaded_video_count"] += 1
                         else:
-                            video_lst[id] = content
-                            video_count += 1
-                            print(video_count)
+                            total_channel_count += 1
+                            channel_lst[channelId] = {
+                                "channelTitle": channelTitle,
+                                "channelId": channelId,
+                                "downloaded_video_count": 1,
+                                "authorised": False,
+                            }
 
-    with open("merged_video_lst.json", "w") as output:
+    # print channels and video count
+    sorted_ids = sorted(
+        channel_lst.keys(),
+        key=lambda x: channel_lst[x]["downloaded_video_count"],
+        reverse=True,
+    )
+    print("\nchannelTitle: downloaded_video_count\n")
+    for id in sorted_ids:
+        print(
+            f"{channel_lst[id]['channelTitle']}: {channel_lst[id]['downloaded_video_count']}"
+        )
+
+    # write to json files
+    merged_video_lst_fp = FOLDER_PATH / "merged_list" / "merged_video_lst.json"
+    channels_lst_fp = FOLDER_PATH / "merged_list" / "channels_lst.json"
+    authorised_video_lst_fp = FOLDER_PATH / "merged_list" / "authorised_video_lst.json"
+
+    with merged_video_lst_fp.open(mode="w") as output:
         json.dump(video_lst, output, indent=4)
+
+    with channels_lst_fp.open(mode="w") as output:
+        json.dump(channel_lst, output, indent=4)
+
+    with authorised_video_lst_fp.open(mode="w") as output:
+        json.dump(authorised_video_lst, output, indent=4)
+
+    print("\n\n\n\n>>>>>>>>")
+    print(f"total_video_count: {total_video_count}")
+    print(f"authorised_video_count: {authorised_video_count}")
+    print(f"total_channel_count: {total_channel_count}")
+    print(f"authorised_channel_count: {authorised_channel_count}")
 
 
 if __name__ == "__main__":
